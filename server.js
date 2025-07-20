8fe8fdc410c4d65114df57fe0c9145ec02b76708
Vinicius Daniel Guimarães <vinicius.daniel.guimaraes0@gmail.com>
2025-07-20 17:48:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const swaggerConfig = require('./swagger');
+// Importação dos módulos necessários
+const express = require('express');  // Framework para criação do servidor
+const swaggerConfig = require('./swagger');  // Configuração do Swagger
 
+// Inicialização do aplicativo Express
 const app = express();
+
+// Configuração do Swagger no aplicativo
 swaggerConfig(app);
 
+// Inicialização do servidor na porta 3000
 app.listen(3000, () => {
   console.log('Swagger UI disponível em http://localhost:3000/api-docs');
 });
